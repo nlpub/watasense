f52269d92c0b813d3cfc2f4d6f8153b2afb410a0
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-08-05 01:36:14 +0500

    Add the PyroVectors proxy and initialize it properly

---
+++ mnogoznal/pyro_vectors.py
@@ -0,0 +1,6 @@
+import Pyro4
+
+Pyro4.config.SERIALIZER = 'pickle'
+
+def PyroVectors(uri):
+    return Pyro4.Proxy(uri)
--- watset_wsd.py
+++ watset_wsd.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from flask import Flask, render_template, send_from_directory, url_for, redirect, request
 from flask_misaka import Misaka
 import mnogoznal
 
 app = Flask(__name__)
 Misaka(app)
+
+if 'PYRO4_W2V' in os.environ:
+    from mnogoznal.pyro_vectors import PyroVectors as PyroVectors
+    w2v = PyroVectors(os.environ['PYRO4_W2V'])
+elif 'W2V_PATH' in os.environ:
+    from gensim.models import KeyedVectors
+    w2v = KeyedVectors.load_word2vec_format(os.environ['W2V_PATH'], binary=True, unicode_errors='ignore')
+    w2v.init_sims(replace=True)
+else:
+    print('No word2vec model is loaded. Please set the PYRO4_W2V or W2V_PATH environment variable.', file=sys.stderr)
 
 # Загрузка базы данных из файла
 #filename = 'D:\Documents\Study\Projects\Python\mnogoznal\watset-mcl-mcl-joint-exp-linked.tsv'
