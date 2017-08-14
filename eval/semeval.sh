0c60781197e5704f250d74dd63b5f01f98090c7b
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-08-14 22:25:52 +0500

    Add baselines and compute fuzzy B³ & NMI

--- eval/semeval.sh
+++ eval/semeval.sh
@@ -6,15 +6,36 @@ for pos in nouns verbs; do
   java -cp cluster-comparison-tools.jar \
     edu.ucla.clustercomparison.FuzzyBCubed \
     $pos.key \
-    $pos.key
+    $pos-baseline-every.key |
+  tee b3-$pos-baseline-every.txt
+
+  java -cp cluster-comparison-tools.jar \
+    edu.ucla.clustercomparison.FuzzyBCubed \
+    $pos.key \
+    $pos-baseline-one.key |
+  tee b3-$pos-baseline-one.txt
+
+  java -cp cluster-comparison-tools.jar \
+    edu.ucla.clustercomparison.FuzzyBCubed \
+    $pos.key \
+    ../${pos}_test.key |
+  tee b3-$pos-test.txt
 
   java -cp cluster-comparison-tools.jar \
     edu.ucla.clustercomparison.FuzzyNormalizedMutualInformation \
     $pos.key \
-    $pos.key
+    $pos-baseline-every.key |
+  tee nmi-$pos-baseline-every.txt
 
   java -cp cluster-comparison-tools.jar \
-    edu.ucla.clustercomparison.FuzzyRandIndex \
+    edu.ucla.clustercomparison.FuzzyNormalizedMutualInformation \
     $pos.key \
-    $pos.key
+    $pos-baseline-one.key |
+  tee nmi-$pos-baseline-one.txt
+
+  java -cp cluster-comparison-tools.jar \
+    edu.ucla.clustercomparison.FuzzyNormalizedMutualInformation \
+    $pos.key \
+    ../${pos}_test.key |
+  tee nmi-$pos-test.txt
 done
