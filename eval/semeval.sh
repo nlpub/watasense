daad5e7c56b9b112995333a993001f0b3c006cfc
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-08-17 04:58:52 +0500

    Better W2V option in semeval.sh

--- eval/semeval.sh
+++ eval/semeval.sh
@@ -3,7 +3,13 @@
 export LANG=en_US.UTF-8 LC_COLLATE=C
 
 INVENTORY="${INVENTORY:-../watset-mcl-mcl-joint-exp-linked.tsv}"
-W2V="${W2V:-../../projlearn/all.norm-sz100-w10-cb0-it1-min100.w2v}"
+
+if [ -z ${W2V_PYRO+x} ]; then
+  W2V_PATH="${W2V_PATH:-../../projlearn/all.norm-sz100-w10-cb0-it1-min100.w2v}"
+  W2V=--w2v=$W2V_PATH
+else
+  W2V=--pyro=$W2V_PYRO
+fi
 
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -11,7 +17,7 @@ which mystem >/dev/null 2>&1 || (echo 'Please make sure that mystem is in PATH.'
 
 for pos in nouns verbs; do
   $CWD/../tests.py --inventory=$INVENTORY --mystem=$(which mystem) --mode=sparse $pos.tsv >$pos-sparse.key
-  $CWD/../tests.py --inventory=$INVENTORY --mystem=$(which mystem) --mode=dense --w2v=$W2V $pos.tsv >$pos-dense.key
+  $CWD/../tests.py --inventory=$INVENTORY --mystem=$(which mystem) --mode=dense $W2V $pos.tsv >$pos-dense.key
 
   for method in one spi sparse dense; do
     java -cp cluster-comparison-tools.jar \
