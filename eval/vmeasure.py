ee455bb9dabe30de02be716162879365fdb13d2c
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-08-18 02:01:25 +0500

    Add the ARI measure to vmeasure.py

--- eval/vmeasure.py
+++ eval/vmeasure.py
@@ -5,9 +5,11 @@ import csv
 from collections import defaultdict, OrderedDict
 from concurrent.futures import ProcessPoolExecutor
 from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
+from sklearn.metrics import adjusted_rand_score
 
 parser = argparse.ArgumentParser(description='SemEval 2010 WSI&D Task V-Measure')
 parser.add_argument('--gold', required=True)
+parser.add_argument('--measure', choices=('vmeasure', 'ari'), default='vmeasure', type=str)
 parser.add_argument('path', nargs='+')
 args = parser.parse_args()
 
@@ -33,7 +35,7 @@ total  = sum(len(values) for values in gold.values())
 def evaluate(path):
     system = systems[path]
 
-    v_measure, scores, clusters_gold, clusters_system = 0., OrderedDict(), [], []
+    measure, scores, clusters_gold, clusters_system = 0., OrderedDict(), [], []
 
     for lemma in lemmas:
         instances = sorted(gold[lemma].keys())
@@ -44,35 +46,43 @@ def evaluate(path):
         clusters_gold   = [senses_gold[gold[lemma][instance]]     for instance in instances]
         clusters_system = [senses_system[system[lemma][instance]] for instance in instances]
 
-        v_measure += v_measure_score(clusters_gold, clusters_system) * len(instances) / total
+        if 'vmeasure' == args.measure:
+            measure += v_measure_score(clusters_gold, clusters_system) * len(instances) / total
 
-        scores[lemma] = (
-            homogeneity_score(clusters_gold, clusters_system),
-            completeness_score(clusters_gold, clusters_system),
-            v_measure_score(clusters_gold, clusters_system)
-        )
+            scores[lemma] = (
+                homogeneity_score(clusters_gold, clusters_system),
+                completeness_score(clusters_gold, clusters_system),
+                v_measure_score(clusters_gold, clusters_system)
+            )
+        else:
+            scores[lemma] = adjusted_rand_score(clusters_gold, clusters_system)
 
-    return v_measure, scores
+            measure += scores[lemma] * len(instances) / total
+
+    return measure, scores
 
 with ProcessPoolExecutor() as executor:
     results = {path: result for path, result in zip(systems, executor.map(evaluate, systems))}
 
-print('\t'.join(('path', 'lemma', 'homogeneity', 'completeness', 'vmeasure')))
-        
-for path, (v_measure, scores) in results.items():
-    print('\t'.join((
-        path,
-        '',
-        '',
-        '',
-        '%.6f' % v_measure
-    )))
+if 'vmeasure' == args.measure:
+    print('\t'.join(('path', 'lemma', 'homogeneity', 'completeness', 'vmeasure')))
 
-    for lemma, (homogeneity, completeness, v_measure) in scores.items():
-        print('\t'.join((
-            path,
-            lemma,
-            '%.6f' % homogeneity,
-            '%.6f' % completeness,
-            '%.6f' % v_measure
-        )))
+    for path, (v_measure, scores) in results.items():
+        print('\t'.join((path, '', '', '', '%.6f' % v_measure)))
+
+        for lemma, (homogeneity, completeness, v_measure) in scores.items():
+            print('\t'.join((
+                path,
+                lemma,
+                '%.6f' % homogeneity,
+                '%.6f' % completeness,
+                '%.6f' % v_measure
+            )))
+else:
+    print('\t'.join(('path', 'lemma', 'ari')))
+
+    for path, (ari, scores) in results.items():
+        print('\t'.join((path, '', '%.6f' % ari)))
+
+        for lemma, ari in scores.items():
+            print('\t'.join((path, lemma, '%.6f' % ari)))
