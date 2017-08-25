dfe7fc334588796c13073592103b0065b8ed51b7
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-08-26 01:58:26 +0500

    Simplify semeval.sh

--- eval/semeval.sh
+++ eval/semeval.sh
@@ -4,6 +4,8 @@ export LANG=en_US.UTF-8 LC_COLLATE=C
 
 INVENTORY="${INVENTORY:-../watset-mcl-mcl-joint-exp-linked.tsv}"
 
+AVERAGE="${AVERAGE:-instances}"
+
 if [ -z ${W2V_PYRO+x} ]; then
   W2V_PATH="${W2V_PATH:-../../projlearn/all.norm-sz100-w10-cb0-it1-min100.w2v}"
   W2V=--w2v=$W2V_PATH
@@ -19,20 +21,12 @@ for pos in nouns verbs; do
   $CWD/../tests.py --inventory=$INVENTORY --mystem=$(which mystem) --mode=sparse $pos.tsv >$pos-sparse.key
   $CWD/../tests.py --inventory=$INVENTORY --mystem=$(which mystem) --mode=dense $W2V $pos.tsv >$pos-dense.key
 
-  for method in one spi sparse dense; do
-    java -cp cluster-comparison-tools.jar \
-      edu.ucla.clustercomparison.FuzzyBCubed \
-      $pos.key $pos-$method.key | tee b3-$pos-$method.txt
-
-    java -cp cluster-comparison-tools.jar \
-      edu.ucla.clustercomparison.FuzzyNormalizedMutualInformation \
-      $pos.key $pos-$method.key | tee nmi-$pos-$method.txt
-
+  for method in sparse dense; do
     files="$files $pos-$method.key"
   done
 
-  ./measure.py --measure=vmeasure --gold=$pos.key $files | tee vm-$pos.tsv
-  ./measure.py --measure=ari      --gold=$pos.key $files | tee ari-$pos.tsv
+  ./measure.py --measure=vmeasure --average=$AVERAGE --gold=$pos.key $files | tee vm-$PREFIX$pos.tsv
+  ./measure.py --measure=ari      --average=$AVERAGE --gold=$pos.key $files | tee ari-$PREFIX$pos.tsv
 
   unset files
 done
